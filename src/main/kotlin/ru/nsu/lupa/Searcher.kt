d334e8e67a01f6b275b8e39ea80cf3e79e217c14
lilvadim <vadimmos2002@gmail.com>
2022-12-10 23:16:08 +0700

    Searcher impl

--- src/main/kotlin/ru/nsu/lupa/Searcher.kt
+++ src/main/kotlin/ru/nsu/lupa/Searcher.kt
@@ -1,4 +1,15 @@
 package ru.nsu.lupa
 
-class Searcher {
+class Searcher(
+    val resources: List<Resource>,
+    val initialProfile: Profile
+) : Runnable {
+
+    var resultProfile = initialProfile
+
+    override fun run() {
+        for (resource in resources) {
+            resultProfile = resource.search(resultProfile)
+        }
+    }
 }
