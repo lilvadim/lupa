e3616853517c7051b63c452340877076775d6289
lilvadim <vadimmos2002@gmail.com>
2022-12-10 23:42:58 +0700

    naive implementation

--- src/main/kotlin/ru/nsu/lupa/Main.kt
+++ src/main/kotlin/ru/nsu/lupa/Main.kt
@@ -1,5 +1,35 @@
 package ru.nsu.lupa
 
-internal fun main(args: Array<String>) {
+import kotlinx.cli.*
+import ru.nsu.lupa.res.VkSearch
 
+val resources = listOf(VkSearch())
+
+internal fun main(args: Array<String>) {
+    val parser = ArgParser("lupa")
+
+    val name by parser.option(
+        type = ArgType.String,
+        fullName = "name",
+        shortName = "n",
+        description = "Any form of name"
+    ).required()
+
+    val surname by parser.option(
+        type = ArgType.String,
+        fullName = "surname",
+        shortName = "s",
+        description = "Surname"
+    ).required()
+
+    parser.parse(args)
+
+    val initialProfile = Profile(
+        name = Name(name),
+        surname = Surname(surname)
+    )
+
+    val searcher = Searcher(resources, initialProfile)
+
+    println(searcher.resultProfile)
 }
