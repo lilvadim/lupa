a0513d8fc9dde5e0acf3455ebfc313dd00e8ab47
lilvadim <vadimmos2002@gmail.com>
2022-12-18 16:51:53 +0700

    update

--- src/main/kotlin/ru/nsu/lupa/NameProcessor.kt
+++ src/main/kotlin/ru/nsu/lupa/NameProcessor.kt
@@ -10,3 +10,10 @@ interface NameProcessor {
     fun synonymsOf(name: Name): List<Name>
 }
 
+/**
+ * Returns name processor which doesn't find any synonymic forms
+ */
+fun simpleNameProcessor() = object : NameProcessor {
+    override fun synonymsOf(name: Name): List<Name> = listOf(name)
+}
+
--- src/main/kotlin/ru/nsu/lupa/res/VkSearch.kt
+++ src/main/kotlin/ru/nsu/lupa/res/VkSearch.kt
@@ -1,10 +1,34 @@
 package ru.nsu.lupa.res
 
+import com.vk.api.sdk.client.VkApiClient
+import com.vk.api.sdk.client.actors.Actor
+import com.vk.api.sdk.client.actors.UserActor
+import com.vk.api.sdk.httpclient.HttpTransportClient
+import com.vk.api.sdk.objects.UserAuthResponse
+import com.vk.api.sdk.queries.users.UsersSearchQuery
 import ru.nsu.lupa.MatchGraph
 import ru.nsu.lupa.Resource
 
+
 class VkSearch : Resource.BaseResource(homeUrl = "https://vk.com/") {
     override fun performSearch(matchGraph: MatchGraph) {
+        val usersSearchQuery = UsersSearchQuery(vkClient, actor)
+    }
 
+    private companion object {
+        const val APP_ID = 51506122
+        const val CLIENT_SECRET = ""
+        const val REDIRECT_URI = ""
+        const val code = ""
+
+        val vkClient = VkApiClient(HttpTransportClient())
+        val actor: UserActor
+        init {
+            val authResponse: UserAuthResponse = vkClient.oAuth()
+                .userAuthorizationCodeFlow(APP_ID, CLIENT_SECRET, REDIRECT_URI, code)
+                .execute()
+
+            actor = UserActor(authResponse.userId, authResponse.accessToken)
+        }
     }
 }
