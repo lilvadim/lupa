4022e823ae8277b58c3a2db764b303d4dde2d9b9
SiegKlass <plugmihail@gmail.com>
2022-12-30 05:16:13 +0700

    upd

--- src/main/kotlin/ru/nsu/lupa/ChainProcessor.kt
+++ src/main/kotlin/ru/nsu/lupa/ChainProcessor.kt
@@ -1,14 +1,16 @@
 package ru.nsu.lupa
 
+import kotlin.math.max
+
 class ChainProcessor: ResultProcessor {
     /**
      * Convert match graph to list of chains from longest to shortest
      */
     private lateinit var g: Map<Profile, Map<Profile, Set<MatchCriteria>>>
 
-    private lateinit var chainList: MutableList<ChainNode<Set<MatchCriteria>, Profile>>
-
     private lateinit var rootChainNode: ChainNode<Set<MatchCriteria>, Profile>
+
+    private var chainList = mutableListOf<Pair<ChainNode<Set<MatchCriteria>, Profile>, Int>>()
 
     override fun process(matchGraph: MatchGraph, rootProfile: Profile): List<ChainNode<Set<MatchCriteria>, Profile>> {
         chainList = mutableListOf()
@@ -18,18 +20,20 @@ class ChainProcessor: ResultProcessor {
             return emptyList()
         }
         dfs(rootProfile, rootChainNode)
-        return chainList
+        return chainList.sortedBy { (_, len) -> len }.map { (chain, _) -> chain }
     }
 
     private fun dfs(node: Profile, previousChainNode: ChainNode<Set<MatchCriteria>, Profile>) {
         if (g[node]!!.isEmpty()) {
             val rootChainNodeCopy = rootChainNode.copy()
             var currentChainNode = rootChainNodeCopy
+            var currentLength = 0
             while (currentChainNode.next != null) {
                 currentChainNode.next = currentChainNode.next!!.copy()
                 currentChainNode = currentChainNode.next!!
+                currentLength++
             }
-            chainList.add(rootChainNodeCopy.next!!)
+            chainList.add(Pair(rootChainNodeCopy.next!!, currentLength))
             return
         }
         for ((key, value) in g[node]!!) {
